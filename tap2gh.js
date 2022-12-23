b4bc2817d40c7e9dc8298752b2a0c205ccbd9b99
Akira Sawada <akira@peatix.com>
2022-12-23 18:20:37 +0900

    Hmm...? inside js file it can't require npm modules?

--- tap2gh.js
+++ tap2gh.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs')
-const glob = require('glob')
-const parser = require('tap-parser')
 
-function run () {
+function run (glob, parser) {
   glob("tapout/t/*.t", (er, files) => {
     const annotations = []
     let pass = 0
