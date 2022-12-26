1e07946558b95980690043222850dbba16eea2b5
Akira Sawada <akira@peatix.com>
2022-12-27 07:38:07 +0900

    missing commas

--- helloworld/index.js
+++ helloworld/index.js
@@ -4,31 +4,31 @@ const yaml = require('yaml')
 const data = [
   {
     name: "Perl Test (1)",
-    pass: 4
+    pass: 4,
     fail: 1,
     ok: false,
   },
   {
     name: "Perl Test (2)",
-    pass: 32
+    pass: 32,
     fail: 0,
     ok: true,
   },
   {
     name: "Perl Test (3)",
-    pass: 414
+    pass: 414,
     fail: 0,
     ok: true,
   },
   {
     name: "Jest Test",
-    pass: 4
+    pass: 4,
     fail: 1,
     ok: false,
   },
   {
     name: "Frontend Lint",
-    pass: 4
+    pass: 4,
     fail: 0,
     ok: true,
   }
