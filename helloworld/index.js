43ec6014fd2873e59f299714dc1cbdb3b8980dd1
Akira Sawada <akira@peatix.com>
2022-12-26 09:37:12 +0900

    test

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,3 +1,3 @@
 console.log('BooBoo');
 const yaml = require('yaml')
-module.exports = function () { console.log(yaml.stringify({ aaa: 42, bbb: { c: 42, d: [ 1,2,3,4,5]}})) }
+module.exports = function () { console.log(yaml.stringify({ aaa: 42, bbb: { c: 42, d: [ 1,2,3,4,6]}})) }
